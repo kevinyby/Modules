b10545b6cf19e1226b16e9f8d98a3d85944073e1
isaacselement <isaacselement@gmail.com>
2013-12-09 16:52:33 +0800

    litlle fixed

--- Data/HTTP/HTTPPostRequest.m
+++ Data/HTTP/HTTPPostRequest.m
@@ -4,24 +4,36 @@
 
 #pragma mark - Overwrite Methods
 -(void) applyRequest:(NSMutableURLRequest*)request parameters:(NSDictionary*)parameters {
+    NSMutableData* requestData = [NSMutableData data];
     
-    NSMutableString* parameterString = [NSMutableString stringWithCapacity:0];
+    NSDictionary* formParameters = [parameters objectForKey: POST_FormParameters];
+    NSData* postNSData = [parameters objectForKey: POST_Data];
     
-    BOOL andFlag = NO;
-    for(NSString* key in parameters) {
-        NSString* value = [parameters objectForKey: key];
-        if(!andFlag) {
-            [parameterString appendFormat: @"%@=%@", key, value];
-            andFlag = YES;
-        } else {
-            [parameterString appendFormat: @"&%@=%@", key, value];
+    if (! formParameters && ! postNSData) formParameters = parameters;          // if no POST_Data & POST_FormParameters specified , regard it as  formParameters;
+    
+    if (formParameters) {
+        
+        NSMutableString* parameterString = [NSMutableString stringWithCapacity:0];
+        
+        BOOL andFlag = NO;
+        for(NSString* key in formParameters) {
+            NSString* value = [formParameters objectForKey: key];
+            if(!andFlag) {
+                [parameterString appendFormat: @"%@=%@", key, value];
+                andFlag = YES;
+            } else {
+                [parameterString appendFormat: @"&%@=%@", key, value];
+            }
         }
+        
+        [requestData appendData: [parameterString dataUsingEncoding: NSUTF8StringEncoding]];
+        [request setValue: @"application/x-www-form-urlencoded" forHTTPHeaderField:@"Content-Type"];
     }
     
+    if (postNSData) [requestData appendData: postNSData];
     
-    NSMutableData* requestData = [NSMutableData dataWithData: [parameterString dataUsingEncoding: NSUTF8StringEncoding]];
+    
     [request setHTTPMethod:@"POST"];
-    [request setValue: @"application/x-www-form-urlencoded" forHTTPHeaderField:@"Content-Type"];
     [request setHTTPBody: requestData];
 }
 
