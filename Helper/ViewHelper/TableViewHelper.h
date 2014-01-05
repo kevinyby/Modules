ca3d3888a589f820c85f3024e14f66d1b2a101cc
isaacselement <isaacselement@gmail.com>
2014-01-05 15:24:29 +0800

    girl girl my girl

--- Helper/ViewHelper/TableViewHelper.h
+++ Helper/ViewHelper/TableViewHelper.h
@@ -6,12 +6,8 @@
 
 +(NSIndexPath*) getLastIndexPath: (UITableView*)tableView inSection: (NSUInteger)section;
 
-<<<<<<< HEAD
 +(NSIndexPath*) getIndexPath: (UITableView*)tableView cellSubView:(UIView*)subview;
-=======
 
-+(NSIndexPath*) getIndexPath: (UITableView*)tableView cellSubView:(UIView*)subview;
 +(UITableViewCell*) getTableViewCell: (UITableView*)tableView cellSubView:(UIView*)subview;
 
->>>>>>> e7a8057204d73f85ad73bd5034f4b6d7b4849eaa
 @end
