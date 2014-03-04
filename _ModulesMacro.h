909613b4a1a42a498cfd887885a0e7ce20fc64d6
isaacselement <isaacselement@gmail.com>
2014-03-04 09:15:13 +0800

    new versiont

--- _ModulesMacro.h
+++ _ModulesMacro.h
@@ -4,7 +4,11 @@
 // And Other Variable : sel_getName(_cmd) ,  __FILE__ , __FUNCTION__ , __PRETTY_FUNCTION__ , __LINE__
 
 #ifdef DEBUG
+
 #define DLOG(_format,args...)  printf("%s (%d):   %s\n",__PRETTY_FUNCTION__ , __LINE__,[[NSString stringWithFormat:(_format),##args] UTF8String])
+
 #else
+
 #define DLOG(_format,args...)
+
 #endif
